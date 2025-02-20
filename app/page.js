034135a907b06a9b423559ec0b30cdc1546d60b9
Darrell CHUA <j22038317@student.newinti.edu.my>
2025-02-21 05:12:00 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import AboutMe from "@/components/home/AboutMe";
 import Contact from "@/components/home/Contact";
 import Intro from "@/components/home/Intro";
 import Project from "@/components/home/Project";
+import SkillsSection from "@/components/home/SkillsSection";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <div>
         <Intro />
         <AboutMe />
+        <SkillsSection />
         <Project />
         <Contact />
       </div>
