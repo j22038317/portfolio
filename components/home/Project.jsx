521d400e455965faa469b92a3f51f527d3b2d604
Darrell CHUA <j22038317@student.newinti.edu.my>
2025-02-21 05:11:57 +0800

    Update Project.jsx

--- components/home/Project.jsx
+++ components/home/Project.jsx
@@ -5,9 +5,8 @@ const Project = () => {
     <main id="project" className="min-h-screen flex items-center justify-center bg-[#4a5c6a] text-white">
       <div className="text-center">
         <h1 className="text-4xl font-bold">Project</h1>
-        <p className="mt-4 text-lg">
-          Hi, I'm [Your Name]. I'm a passionate developer who loves building web applications.
-        </p>
+      </div>
+      <div>
       </div>
     </main>
   );
