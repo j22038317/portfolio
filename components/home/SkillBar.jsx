0036d20845384a992a41fd0ff0b6787e4ff73a9f
Darrell CHUA <j22038317@student.newinti.edu.my>
2025-02-21 05:35:42 +0800

    Updated Skills

--- components/home/SkillBar.jsx
+++ components/home/SkillBar.jsx
@@ -1,15 +1,43 @@
-import React from 'react';
+"use client";
+import React, { useEffect, useRef, useState } from 'react';
 
 const SkillBar = ({ skill, level }) => {
+  const [width, setWidth] = useState(0);
+  const skillBarRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setWidth(level);
+          }
+        });
+      },
+      { threshold: 0.1 }
+    );
+
+    if (skillBarRef.current) {
+      observer.observe(skillBarRef.current);
+    }
+
+    // 清理 observer
+    return () => {
+      if (skillBarRef.current) {
+        observer.unobserve(skillBarRef.current);
+      }
+    };
+  }, [level]);
+
   return (
-    <div className="w-full bg-gray-200 rounded-full mb-4">
+    <div className="w-full bg-gray-200 rounded-xl mb-4" ref={skillBarRef}>
       <div className="flex justify-between items-center px-4 py-2">
-        <span className="font-bold text-gray-800">{skill}</span>
+        <span className="font-bold text-dark-mode">{skill}</span>
         <span className="text-sm text-gray-600">{level}%</span>
       </div>
       <div
-        className="h-2 bg-blue-500 rounded-full"
-        style={{ width: `${level}%`, transition: 'width 2s ease-in-out' }}
+        className="h-2 bg-blue-600 rounded-full transition-all duration-1000 ease-in-out"
+        style={{ width: `${width}%` }}
       ></div>
     </div>
   );
--- components/home/SkillsSection.jsx
+++ components/home/SkillsSection.jsx
@@ -3,19 +3,19 @@ import SkillBar from './SkillBar';
 
 const SkillsSection = () => {
   const skills = [
-    { skill: 'Python', level: 90 },
-    { skill: 'SQL', level: 85 },
-    { skill: 'Data Structures', level: 80 },
-    { skill: 'C/C++', level: 75 },
-    { skill: 'JavaScript', level: 90 },
-    { skill: 'React', level: 85 },
-    { skill: 'HTML/CSS', level: 95 },
-    { skill: 'C#', level: 70 },
+    { skill: 'C++', level: 90 },
+    { skill: 'Python', level: 80 },
+    { skill: 'Java', level:78 },
+    { skill: 'Javascript/HTML/CSS', level: 60 },
+    { skill: 'React', level: 55 },
+    { skill: 'Nextjs', level: 50 },
+    { skill: 'Database', level: 30 },
+    { skill: 'Threejs/GSAP', level: 15 },
   ];
 
   return (
-    <section id="skills" className="p-8 bg-gray-100 dark:bg-gray-800">
-      <h2 className="text-3xl font-bold text-center mb-8 text-gray-900 dark:text-white">
+    <section id="skills" className="p-8 bg-light-mode dark:bg-dark-mode">
+      <h2 className="text-3xl font-bold text-center mb-8 text-dark-mode dark:text-light-mode">
         Skills
       </h2>
       <div className="max-w-2xl mx-auto">
