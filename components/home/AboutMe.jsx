ef90f8a70832b0d2f3d724aca01b2c0b6b1806e5
Darrell CHUA <j22038317@student.newinti.edu.my>
2025-02-21 04:38:31 +0800

    Update AboutMe.jsx

--- components/home/AboutMe.jsx
+++ components/home/AboutMe.jsx
@@ -24,6 +24,15 @@ const AboutMe = () => {
           my backend development skills. I’m all about growth, learning, and creating meaningful digital
           experiences!
         </p>
+        <div className="mt-8">
+          <a
+            href="/Darrell Chua Resume.pdf"
+            download="Darrell_Chua_Resume.pdf"
+            className="inline-block bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-950 transition duration-300"
+          >
+            My Résumé
+          </a>
+        </div>
       </div>
     </main>
   );
